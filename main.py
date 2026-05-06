fdec756347785af48c4d343f3655a4ab7c39c645
j2nnifer <jekuei@ucsd.edu>
2026-05-06 22:35:42 +0000

    Test pre-commit

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-
-import os  # Rule F401: Unused import
-import sys # Rule F401: Unused import
-
-def main():
-    unused_var = 42
-    print("super long string lol aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    print("Hello from dsc190assgn05!")
-
-
-if __name__ == "__main__":
-    main()
+x = 1 + 2
+print(x)
