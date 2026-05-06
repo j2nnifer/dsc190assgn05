2717b7264f36073dcbf9f0672b6f917d94a4ce60
j2nnifer <jekuei@ucsd.edu>
2026-05-06 22:25:59 +0000

    step6

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+
+import os  # Rule F401: Unused import
+import sys # Rule F401: Unused import
+
 def main():
+    unused_var = 42
+    print("super long string lol aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     print("Hello from dsc190assgn05!")
 
 
